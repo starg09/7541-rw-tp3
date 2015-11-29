d660318697dc12cf77cf205fa987be0d64a9b763
Guillermo Bianchi <ultraguille2006@gmail.com>
2015-11-29 20:29:14 -0300

    Armadas funciones de parseo

---
+++ carga_archivos.py
@@ -0,0 +1,6 @@
+from estructuras import *
+from parseo_csv import *
+
+def cargar_ciudades(ruta):
+	#TODO
+	pass
--- parseo_csv.py
+++ parseo_csv.py
@@ -1,5 +1,18 @@
-def parsear_ciudad(linea, ciudades):
+from estructuras import *
+
+def parsear_ciudad(linea):
 	"""
-	Lee una linea del archivo de ciudades, separa los campos, y los almacena en la posicion apropiada del diccionario
+	Lee una linea del archivo de ciudades, separa los campos, y devuelve un objeto armado.
 	"""
 	campos = linea.split("	")
+	nueva_ciudad = new Ciudad(campos[0], campos[1], campos[2], campos[3], campos[4], campos[5])
+	return nueva_ciudad
+
+
+def parsear_ruta(linea):
+	"""
+	Lee una linea del archivo de rutas, separa los campos, y devuelve un objeto armado.
+	"""
+	campos = linea.split("	")
+	nueva_ruta = new Ruta(campos[0], campos[1], campos[2], campos[3], campos[4])
+	return nueva_ruta
