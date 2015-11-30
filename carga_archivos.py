567c06f987749c590f4412e900d9ff00633917da
starg09 <ultraguille2006@gmail.com>
2015-11-30 22:35:25 +0000

    Armadas funciones de carga de archivos

--- carga_archivos.py
+++ carga_archivos.py
@@ -1,6 +1,58 @@
 from estructuras import *
 from parseo_csv import *
 
-def cargar_ciudades(ruta):
-	#TODO
-	pass
+def cargar_ciudades(ruta_archivo):
+	"""
+	Carga todas las ciudades en el csv dado, y devuelve un diccionario con los objetos Ciudad creados apropiadamente
+	Si alguna ciudad carga erroneamente, devuelve Null.
+	"""
+	archivo = open(ruta_archivo, "r")
+
+	#Pasar la primer linea
+	read(archivo)
+	linea = read(archivo)
+	
+	ciudades = {}
+	while not (linea == ""):
+		nueva_ciudad = parsear_ciudad(linea)
+		if (nueva_ciudad == Null):
+			del ciudades
+			return Null
+		ciudades[nueva_ciudad.id] = nueva_ciudad
+		linea = read(archivo)
+	archivo.close()
+
+	return ciudades
+
+
+
+def cargar_rutas(ruta_archivo):
+	"""
+	Carga todas las rutas en el csv dado, y devuelve un diccionario con los objetos
+	rutas creados apropiadamente.
+
+	Para acceder a un elemento de este diccionario, se debe buscar en la forma
+	"rutas[ciudad_A][ciudad_B]", siendo ciudad_A la ciudad de menor ID.
+
+	Si alguna ciudad carga erroneamente, la función devuelve Null.
+	"""
+	archivo = open(ruta_archivo, "r")
+
+	#Pasar la primer linea
+	read(archivo)
+	linea = read(archivo)
+	
+	rutas = {}
+	while not (linea == ""):
+		nueva_ruta = parsear_ruta(linea)
+		if (nueva_ruta == Null):
+			del rutas
+			return Null
+		if (nueva_ruta.ciudades[0] < nueva_ruta.ciudades[1]):
+			rutas[nueva_ruta.ciudades[0]][nueva_ruta.ciudades[1]] = nueva_ruta
+		else:
+			rutas[nueva_ruta.ciudades[1]][nueva_ruta.ciudades[0]] = nueva_ruta
+		linea = read(archivo)
+	archivo.close()
+
+	return rutas
--- parseo_csv.py
+++ parseo_csv.py
@@ -2,17 +2,33 @@ from estructuras import *
 
 def parsear_ciudad(linea):
 	"""
-	Lee una linea del archivo de ciudades, separa los campos, y devuelve un objeto armado.
+	Lee una linea del archivo de ciudades, separa los campos, y devuelve un objeto Ciudad armado apropiadamente.
+	Si hay un error al aplicar split, y hay menos campos de los esperados, devuelve Null.
 	"""
-	campos = linea.split("	")
-	nueva_ciudad = new Ciudad(campos[0], campos[1], campos[2], campos[3], campos[4], campos[5])
+	campos = linea.split("\t")
+	if (len(campos) < 6):
+		return Null
+	try:
+		nueva_ciudad = Ciudad(int(campos[0]), campos[1], float(campos[2]), float(campos[3]), int(campos[4]), int(campos[5]))
+	except ValueError:
+		return Null
 	return nueva_ciudad
 
 
 def parsear_ruta(linea):
 	"""
-	Lee una linea del archivo de rutas, separa los campos, y devuelve un objeto armado.
+	Lee una linea del archivo de rutas, separa los campos, y devuelve un objeto Ruta armado apropiadamente.
+	Si hay un error al aplicar split, y hay menos campos de los esperados, devuelve Null.
+	Si algun valor no tiene el formato apropiado (documentado en la clase) devuelve Null.
+	Si la ciudad de destino es igual a la de llegada, devuelve Null.
 	"""
-	campos = linea.split("	")
-	nueva_ruta = new Ruta(campos[0], campos[1], campos[2], campos[3], campos[4])
+	campos = linea.split("\t")
+	if (len(campos) < 5):
+		return Null
+	try:
+		if (int(campos[1]) == int(campos[2])):
+			return Null
+		nueva_ruta = rutas(int(campos[0]), int(campos[1]), int(campos[2]), int(campos[3]), float(campos[4]))
+	except ValueError:
+		return Null
 	return nueva_ruta
