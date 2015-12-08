05efc8561342251226bbc9c8d5e15661dd31a204
Guillermo Bianchi <ultraguille2006@gmail.com>
2015-12-08 20:39:55 -0300

    Pqueña prueba agregada temporalmente en main

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from carga_archivos import *
+from ruta_eficiente import *
+
 def main():
-	
+	ciudades = cargar_ciudades("/home/starg09/Desktop/GitHub/7541-rw-tp3/Archivos/ciudades.csv")
+	rutas, grafo = cargar_rutas("/home/starg09/Desktop/GitHub/7541-rw-tp3/Archivos/rutas.csv", ciudades)
+	print camino_minimo(13, 28, rutas, ciudades)
+
+main()
